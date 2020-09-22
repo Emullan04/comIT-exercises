44ee9e030eb89ef014fbbd2efe42e59bdbf0c0ab
Emullan04 <nallum.acire@gmail.com>
2020-09-21 20:48:27 -0500

    -added exercise 19

---
+++ Exercise 19/index.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const BIRTH_MONTH = "August";
+const FIRST_NAME = "Erica";
+const LAST_NAME = "Mullan";
+
+let city;
+let age;
+let nextCity;
+
+city = "Winnipeg";
+age = 29;
+nextCity = city;
+
+console.log("Birth Month: ", BIRTH_MONTH);
+console.log("First Name: ", FIRST_NAME);
+console.log("Last Name: ", LAST_NAME);
+console.log("City: ", city);
+console.log("Age: ", age);
+console.log("Next City: ", nextCity);
