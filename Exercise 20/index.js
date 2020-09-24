85ebd5e03445d187c2f01b9004f56c0ee50f2c2b
Emullan04 <nallum.acire@gmail.com>
2020-09-23 19:45:43 -0500

    -added exercise 20

---
+++ Exercise 20/index.js
@@ -0,0 +1,24 @@
+age = 20;
+firstName = "Erica";
+finishedCourse = false;
+finalScore = null;
+winningNumbers;
+
+
+console.log("Current Date Type of age is:", age);
+console.log("Current Date Type of firstName is:", firstName);
+console.log("Current Date Type of finishedCourse is:", finishedCourse);
+console.log("Current Date Type of finalScore is:", finalScore);
+console.log("Current Date Type of winningNumbers is:", winningNumbers);
+
+newAge = String(age);
+newFirstName = Boolean(firstName);
+newFinishedCourse = Number(finishedCourse);
+newFinalScore = Boolean(finalScore);
+newWinningNumbers = Number(winningNumbers);
+
+console.log("New Data Type of age is:", newAge);
+console.log("New Data Type of firstName is:", newFirstName);
+console.log("New Data Type of finishedCourse is:", newFinishedCourse);
+console.log("New Data Type of finalScore is:", newFinalScore);
+console.log("New Data Type of winningNumbers is:", newWinningNumbers);
